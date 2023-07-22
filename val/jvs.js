c2d44dbec27fe8b7a9201698722074f828572e6d
gyanqui <gyanqui3@gmail.com>
2023-07-22 12:46:51 -0500

    Clean up code

--- val/jvs.js
+++
@@ -1,6 +0,0 @@
-var change = document.getElementById("boton")
-change.addEventListener("click", cambiodepantalla)
-function cambiodepantalla(){
-    document.getElementById("val").style.display = "none";
-    document.getElementById("gab").style.display = "block";
-}
